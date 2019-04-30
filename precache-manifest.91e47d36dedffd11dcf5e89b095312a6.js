bf66fa0b1abb8acefa433a2ab7cc30ab0983ab09
PavelMartinez <golovip2014@mail.ru>
2019-04-30 22:26:32 +0300

    Updates

---
+++ precache-manifest.91e47d36dedffd11dcf5e89b095312a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f80bdb2638f2a2c041f7",
+    "url": "/vkapp/static/js/runtime~main.f80bdb26.js"
+  },
+  {
+    "revision": "16489776e5705f9c07be",
+    "url": "/vkapp/static/js/main.16489776.chunk.js"
+  },
+  {
+    "revision": "659b95854ee489ac9c64",
+    "url": "/vkapp/static/js/2.659b9585.chunk.js"
+  },
+  {
+    "revision": "659b95854ee489ac9c64",
+    "url": "/vkapp/static/css/2.b6c061f4.chunk.css"
+  },
+  {
+    "revision": "41cb38a52d29db12efd7947d878531f8",
+    "url": "/vkapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vkapp/precache-manifest.3bf16019b751362dbd8af28d0b6029a5.js"
+  "/vkapp/precache-manifest.91e47d36dedffd11dcf5e89b095312a6.js"
 );
 
 workbox.clientsClaim();

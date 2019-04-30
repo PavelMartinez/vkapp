a09be515d42e0c4fb3442861ae7e6e3aa66a6336
PavelMartinez <golovip2014@mail.ru>
2019-04-30 19:35:54 +0300

    Updates

---
+++ precache-manifest.2bc558eed4b9f93bc21c2e9b509e63aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapp/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "f80bdb2638f2a2c041f7",
+    "url": "/vkapp/static/js/runtime~main.f80bdb26.js"
+  },
+  {
+    "revision": "76d54f15312ff1924fd8",
+    "url": "/vkapp/static/js/main.76d54f15.chunk.js"
+  },
+  {
+    "revision": "4e5913f3a550011ef76a",
+    "url": "/vkapp/static/js/2.4e5913f3.chunk.js"
+  },
+  {
+    "revision": "76d54f15312ff1924fd8",
+    "url": "/vkapp/static/css/main.aefb9e22.chunk.css"
+  },
+  {
+    "revision": "4e5913f3a550011ef76a",
+    "url": "/vkapp/static/css/2.4333d0ee.chunk.css"
+  },
+  {
+    "revision": "f24a715c196340126d8ad3cb632d7a51",
+    "url": "/vkapp/index.html"
+  }
+];
